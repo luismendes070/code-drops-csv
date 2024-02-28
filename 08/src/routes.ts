dfc5276f5856bcee4bd111eb43805e37564b0db0
Luis Mendes <machadoluismendes@gmail.com>
2024-02-28 08:18:06 -0300

    Initial commit
    MongoDB schema.prisma
    Dockerfile

---
+++ 08/src/routes.ts
@@ -0,0 +1,21 @@
+import { Request, Response, Router } from "express";
+
+import multer from "multer";
+
+const multerConfig = multer();
+
+const router = Router();
+
+router.post(
+    "/products",
+    multerConfig.single("file"),
+    (request: Request, response: Response) => {
+
+        console.log(request.file.buffer.toString("utf-8"));
+        return response.send();
+
+    }
+);
+
+export { router };
+
