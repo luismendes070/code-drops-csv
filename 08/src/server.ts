ecc379e2d820390a094ae8fb43d1aadfb3eda8fd
Luis Mendes <machadoluismendes@gmail.com>
2024-03-08 16:36:49 -0300

    Increment
    cors

--- 08/src/server.ts
+++ 08/src/server.ts
@@ -1,7 +1,18 @@
 import express from "express";
 import { router } from "./routes";
 
-const app = express();
+import cors from 'cors';
+const app = express()
+ 
+app.use(cors())
+ 
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 app.use(router);
 
