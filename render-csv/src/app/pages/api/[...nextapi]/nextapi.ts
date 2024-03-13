a2c838e2b2e8cf55a6669bcdaf89ca272a07f3a2
Luis Mendes <machadoluismendes@gmail.com>
2024-03-13 13:33:28 -0300

    Increment
    npm update http-proxy-middleware

--- render-csv/src/app/pages/api/[...nextapi]/nextapi.ts
+++ render-csv/src/app/pages/api/[...nextapi]/nextapi.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import httpProxyMiddleware from 'http-proxy-middleware';
 
 const proxy = httpProxyMiddleware({
-  target: 'http://localhost:3000/your-csv-route', // Replace with your server port
+  target: 'http://localhost:3001/your-csv-route', // Replace with your server port
   pathRewrite: { '^/api': '' }, // Remove '/api' prefix from incoming requests
 });
 
